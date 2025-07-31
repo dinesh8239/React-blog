af44486da0857cce9a52e9d54a4bef921b99fd73
Dinesh <dkkumar97855@gmail.com>
2025-07-31 11:53:23 +0530

    refactor: update App component structure and styling for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,34 @@
 // import { useState } from 'react'
 import User from './Header.jsx'
 import './App.css'
-import Name, {Profile, Setting, Sign} from './UserComponent.jsx'
+import Name, { Profile, Setting, Sign } from './UserComponent.jsx'
 
 
 function App() {
-  const userName = "John Snow"
+  const userName = "Anil Sidhu Todos"
   const a = 20;
   const b = 50;
   return (
-    <div>
-      <h1>first Component</h1>
-      <City/>
-      <Profession/>
-      <Name/>
-      <Profile/>
-      <Setting/>
-      <h1>{Sign}</h1>
+    <div className="container">
       <h1>{userName}</h1>
-      <h2>{a+b}</h2>
-      {10+20+30}
+      <img 
+             src="https://images.unsplash.com/25/puppy-love.jpg?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmxhY2slMjBwdXBweXxlbnwwfHwwfHx8MA%3D%3D"
+        alt="dog image"
+        className="photo"
+      />
+      <ul>
+        <li><b>Invent new traffic lights</b></li>
+        <li><b>Rehearse a movie scene</b></li>
+        <li><b>Improve the spectrum technology</b></li>
+      </ul>
+      <button onClick={callFun}>Click Me</button>
     </div>
   )
 }
 
-function City() {
-  return (
-    <h2>Bengaluru</h2>
-  )
+function callFun() {
+  alert("function called")
 }
 
-function Profession() {
-  return (
-    <h3>
-      Software Engineer
-    </h3>
-  )
-}
+
 export default App
